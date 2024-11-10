a6884d254b55ea2ff76db6bb2625208f13fda8b2
mvp1717 <vyacheslav.kryvosheya@capgemini.com>
2024-11-10 13:14:50 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,4 +36,16 @@ resource "azurerm_app_service_plan" "sp1" {
   }
 }
 
+resource "azurerm_app_service" "website" {
+  name                = var.web_app_name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.wsdevops.name
+  app_service_plan_id = azurerm_app_service_plan.sp1.id
+
+  site_config {
+    linux_fx_version = "NODE|22-lts"
+    scm_type         = "LocalGit"
+  }
+}
+
 
